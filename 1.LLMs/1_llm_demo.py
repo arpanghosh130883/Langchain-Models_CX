150eeb969869003d75bb4c4cda43fcbd09f1a42c
Arpan Ghosh <70130254+arpanghosh130883@users.noreply.github.com>
2025-05-28 07:58:12 +0530

    Update 1_llm_demo.py

--- 1.LLMs/1_llm_demo.py
+++ 1.LLMs/1_llm_demo.py
@@ -3,7 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = OpenAI(model='gpt-3.5-turbo-instruct')
+# Get the API key from the environment
+api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize the OpenAI LLM
+llm = OpenAI(api_key=api_key, model='gpt-3.5-turbo-instruct')
+
+#llm = OpenAI(model='gpt-3.5-turbo-instruct')
 
 result = llm.invoke("What is the capital of India")
 
